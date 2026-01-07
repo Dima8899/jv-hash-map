2af94ae8420b0b03aa688712ffef32a51ae6a42c
Dima Babenko <klapster1@gmail.com>
2026-01-07 22:41:13 +0300

    all methods put getValue and getSize created

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -4,77 +4,85 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75f;
 
-    private Node<K, V>[] table;
+    private Node[] table;
     private int size;
 
-        public boolean keyEquals(K k1, K k2) {
-            return k1 == null ? k2 == null : k1.equals(k2);
-        }
-        public void resize() {
-            Node<K, V>[] oldTable = table;
-            table = new Node[oldTable.length * 2];
-            size = 0;
-
-            for (Node<K, V> node : oldTable) {
-                while (node != null){
-                    put(node.key, node.value);
-                    node = node.next;
-                }
-            }
+    public boolean keyEquals(K k1, K k2) {
+        return k1 == null ? k2 == null : k1.equals(k2);
     }
 
+    public void resize() {
+        Node<K, V>[] oldTable = table;
+        table = new Node[oldTable.length * 2];
+        size = 0;
 
+        for (Node<K, V> node : oldTable) {
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
+            }
+        }
+    }
 
     @Override
     public void put(K key, V value) {
-         if (table == null) {
-             table = new Node<K,V>;
-         }
-         if (size >= table.length * LOAD_FACTOR) {
-             resize();
-         }
-         int hash = key == null ? 0 : key.hashCode();
-         int index = Math.abs(hash % table.length);
+        if (table == null) {
+            table = new Node[DEFAULT_CAPACITY];
+        }
+        if (size >= table.length * LOAD_FACTOR) {
+            resize();
+        }
+        int hash = key == null ? 0 : key.hashCode();
+        int index = Math.abs(hash % table.length);
 
-         Node<K,V> current = table[index];
+        Node<K, V> current = table[index];
 
-         if (current == null) {
-             table[index] = new Node(key, value, null);
-             size ++;
-             return;
-         }
+        if (current == null) {
+            table[index] = new Node(key, value, null);
+            size++;
+            return;
+        }
 
-         Node<K, V> prev = null;
-         while (current != null) {
-             if (keyEquals(current.key, key)) {
-                 current.value = value;
-                 return;
-             }
-             prev = current;
-             current = current.next;
-         }
-         prev.next = new Node(key, value, null);
-         size++;
+        Node<K, V> prev = null;
+        while (current != null) {
+            if (keyEquals(current.key, key)) {
+                current.value = value;
+                return;
+            }
+            prev = current;
+            current = current.next;
+        }
+        prev.next = new Node(key, value, null);
+        size++;
     }
 
     @Override
     public V getValue(K key) {
-        for (Node<K, V> search : table) {
-            if (key.equals(search.key)){
-                return search.value;
-            }
+        if (table == null) {
             return null;
         }
+        int hash = key.hashCode();
+        int index = Math.abs(hash % table.length);
+
+        Node<K, V> val = table[index];
+        while (val != null) {
+            if (keyEquals(key, val.key)) {
+                return val.value;
+            }
+            val = val.next;
+        }
+        return null;
     }
 
     @Override
     public int getSize() {
         return size;
     }
+
     static class Node<K, V> {
-        K key;
-        V value;
-        Node<K, V> next;
+        private K key;
+        private V value;
+        private Node<K, V> next;
 
         Node(K key, V value, Node<K, V> next) {
             this.key = key;
